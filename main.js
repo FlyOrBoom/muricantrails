9469cd9d0bbc0a58ebe09a041a51b320cb9111db
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2022-05-26 19:50:26 -0700

    Moar realistic

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const md = new markdownit({html:true})
 const image = new Image()
 const data = 
   Object.fromEntries(
@@ -7,24 +6,49 @@ const data =
     ).data
 .map(([id,...rest])=>[id,rest]))
 
-const output = document.querySelector('output')
 const endings = {
   'MC': '\n<I>',
   'SA': ' <I>',
   '': '\n <button>PRESS SPACE TO CONTINUE</button>',
   'PURCHASE': '\nYou have $_Money.\nBuy an item: <I>\n<button>PRESS SPACE TO EXIT</button>'
 }
-const variables = {
+
+const v = {
   Version: '2022.05.26.1',
   Money: 1600,
   Items: {}
 }
+
 let input
 let id, prompt, type, variable, choices
 
-function start(_id){
+const format = string => string
+    .replaceAll('\n','\n\n')
+    .replaceAll('<I>','<input placeholder="...">')
+    .replaceAll(/_(\w+)/g, (_,key) => v[key] )
+    .replaceAll(/(?![^\n]{1,56}$)([^\n]{1,56})\s/g, '$1\n')
+
+let w = 1000
+let h = 800
+
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')
+const glcanvas = fx.canvas()
+
+async function initCanvas(){
+  canvas.width = w
+  canvas.height = h
+  document.body.prepend(glcanvas)
+  ctx.translate(w/8,h/8)
+  ctx.scale(3/4,3/4)
+  ctx.textBaseline = 'top'
+}
+
+async function show(_id){
   id = _id;
-  ctx.clearRect(0,0,w,h);
+  ctx.clearRect(0,0,w,h)
+  ctx.fillStyle = '#000'
+  ctx.fillRect(0,0,w,h);
   [prompt, type, variable, ...choices] = data[id]
   console.log(id,data[id],choices)
   image.src = `media/${id}.png`
@@ -33,71 +57,62 @@ function start(_id){
     setTimeout(render,10)
   })
   prompt += endings[type]
-  write((prompt))
-
+  write(prompt)
 }
-function clickListener(){
+
+async function main(){
+  document.addEventListener('keydown',enterListener)
+  document.addEventListener('click',clickListener)
+
+  initCanvas()
+  show('Begin')
+}
+main()
+
+async function clickListener(){
   switch(type){
     case '':
-      start(choices[0])
+      show(choices[0])
       break
     case 'PURCHASE':
-      start(choices[0])
+      show(choices[0])
       break
   }
 }
-function enterListener({key}){
+
+async function enterListener({key}){
   if(type == 'PURCHASE' && key == ' ') return clickListener()
   if(key != 'Enter') return
-  if(variable) variables[variable] = input.value
+  if(variable) v[variable] = input.value
   const num = parseInt(input.value)
   switch(type){
     case 'MC':
       if(choices[num]){
-        start(choices[num])
+        show(choices[num])
       } else{
-        write(('INVALID OPTION. PICK ANOTHER.\n'+prompt))
+        write('INVALID OPTION. PICK ANOTHER.\n'+prompt)
       }
       break
     case 'SA':
-      start(choices[0])
+      show(choices[0])
       break
     case 'PURCHASE':
-      if(variables.Money >= choices[num]) {
-        variables.Money -= choices[num]
+      if(v.Money >= choices[num]) {
+        v.Money -= choices[num]
         write(("YOU BOUGHT ONE ITEM.\n"+prompt))
       } else {
         write(("YOU DON'T HAVE THE MONEY FOR THAT. PICK ANOTHER OR LEAVE MY STORE.\n"+prompt))
       }
 
       
-      //if(item in variables.items) variables.items[item]++
-      //else variables.items[item] = 0
+      //if(item in v.items) variables.items[item]++
+      //else v.items[item] = 0
   }
 
 }
 
-function format(content){
-  return (
-    content
-    .replaceAll('\n','\n\n')
-    .replaceAll('<I>','<input placeholder="...">')
-    .replaceAll(/_(\w+)/g, (_,key) => variables[key] )
-    .replaceAll(/(?![^\n]{1,50}$)([^\n]{1,50})\s/g, '$1\n')
-  )
-}
-let w = 1000
-let h = 800
-const canvas = document.querySelector('canvas')
-let glcanvas = fx.canvas()
-canvas.parentNode.insertBefore(glcanvas,canvas)
-canvas.style.display = 'none'
-const ctx = canvas.getContext('2d')
-ctx.translate(w/10,h/10)
-ctx.scale(4/5,4/5)
-ctx.textBaseline = 'top'
-function write(content){
-  ctx.font = '20px "Press Start 2P"';
+async function write(content){
+  ctx.font = '18px pressstart'
 
   let x = 0
   let y = 20 
@@ -108,11 +123,14 @@ function write(content){
       x = 0
       y += 30
     } else {
-      x += 20
+      x += 18 
       
       setTimeout((_c,_x,_y)=>{
         ctx.fillStyle = 'black'
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 2
         ctx.fillText(_c,_x+4,_y+4)
+        ctx.strokeText(_c,_x,_y)
         ctx.fillStyle = 'white'
         ctx.fillText(_c,_x,_y)
       },i*10,char,x,y)
@@ -128,19 +146,15 @@ function write(content){
 
 }
 
-function render(){
+async function render(){
     let texture = glcanvas.texture(canvas);
     texture.loadContentsOf(canvas);
 
       // Apply WebGL magic
     glcanvas.draw(texture)
-        .bulgePinch(w/2, h/2, w*2/3, 0.15)
+        .bulgePinch(w/2, h/2, w*3/4, 0.25)
         .vignette(0.25, 0.74)
-        .lensBlur(0.2,1,0)
+        //.lensBlur(0.2,1,0)
         .brightnessContrast(0.1,0.1)
         .update();
 }
-document.addEventListener('keydown',enterListener)
-document.addEventListener('click',clickListener)
-start('Begin')
-
